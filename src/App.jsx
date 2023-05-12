6bb983f9c1c947a53fed408456caf46646c88644
Amritansh <amritansh.soni20@gmail.com>
2023-05-12 18:30:30 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ThemeToggle from "./ThemeToggle";
+import SearchForm from "./SearchForm";
+import ImageGallery from "./ImageGallery";
+
+function App() {
+  return (
+    <main>
+      <ThemeToggle></ThemeToggle>
+      <SearchForm></SearchForm>
+      <ImageGallery></ImageGallery>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Context.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AppContext = createContext();
+
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+
+  const storedTheme = localStorage.getItem("dark-theme") === "true";
+
+  return storedTheme || prefersDarkMode;
+};
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("cat");
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    // const body = document.querySelector("body");
+    // body.classList.toggle("dark-theme", newDarkTheme); // useEffect me dependency array me isDarkTheme in dono line ka kaam kr rha.
+
+    localStorage.setItem("dark-theme", newDarkTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
